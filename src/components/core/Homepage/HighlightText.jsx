e53506f19bf573ecd6dc159b30f4be49602cc7bd
Mohammad <mohammadabdullateef@eng.rizvi.edu.in>
2023-06-10 18:18:20 +0530

    Home Page almost complete -review slider and styling remaining

---
+++ src/components/core/Homepage/HighlightText.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function HighlightText({text}) {
+  return (
+    <>
+    <span className='flex text-caribbeangreen-200'>
+      {text}
+    </span>
+    </>
+  )
+}
